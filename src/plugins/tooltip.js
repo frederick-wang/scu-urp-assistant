19a71e710e4f7ae7da7ba7e4797c0c6756f6f33b
Frederick Wang <hwoam@outlook.com>
2018-12-04 15:05:15 +0800

    版本号更新

--- src/plugins/tooltip.js
+++ src/plugins/tooltip.js
@@ -4,7 +4,7 @@ const tooltip = {
   pathname: '/**',
   $loginTooltip: undefined,
   $navTooltip: undefined,
-  version: '0.7.3',
+  version: '0.7.4',
   init () {
     if (window.location.pathname === '/login') {
       this.$loginTooltip = window.$(`
