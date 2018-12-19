c743df3429bc403f14b500ea83f4ef85eee15c13
Frederick Wang <hwoam@outlook.com>
2018-12-20 01:47:19 +0800

    3265个培养方案和指导性教学计划爬取完成。

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
-const data = require('../data/data.json')
+const data = require('../data/fajhh.json')
 
-fs.writeFileSync('data/fajhh.json', JSON.stringify(data.map(v => v['方案计划号']).sort(function (a, b) {
-  return a - b
-})))
+// fs.writeFileSync('data/fajhh.json', JSON.stringify(data.map(v => v['方案计划号']).sort(function (a, b) {
+//   return a - b
+// })))
+console.log(data[710], data[711])
