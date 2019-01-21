f727bcb17218fbf5086d2ed55db5261803f539c8
Frederick Wang <hwoam@outlook.com>
2019-01-22 03:50:58 +0800

    🎫  chore(remove transformed/ from git.):

--- transformed/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-var data = require('../data/fajhh.json');
-
-// fs.writeFileSync('data/fajhh.json', JSON.stringify(data.map(v => v['方案计划号']).sort(function (a, b) {
-//   return a - b
-// })))
-console.log(data[710], data[711]);
