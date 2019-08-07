2b503183af301ee2b5c1fd888c1891327837df92
Zhaoji Wang <hwoam@outlook.com>
2019-08-07 14:27:42 +0800

    fix(scores-information): 修复了使用协议无论确认还是取消都会显示数据的bug

--- src/plugins/scores-information/index.ts
+++ src/plugins/scores-information/index.ts
@@ -14,12 +14,19 @@ function render(root: HTMLElement) {
     <p style="text-indent: 2em;">该页面展示的部分敏感数据（最高分、平均分、最低分、名次）调用了综合教务系统<span style="color: red;">【未公开】的接口</span>，如果综合教务系统关闭了该接口，那么这个功能就报废了，我们将无法再获取到这些教务系统屏蔽的数据！</p>
     <p style="text-indent: 2em;">因此，如果您要用本页面展示的这些敏感数据和您的任课老师沟通，我希望您可以<span style="color: red;">不要透露该插件的存在</span>，只是说这些敏感数据是您私下联系同学们询问成绩，从而获得的调查结果！</p>
     <p style="text-indent: 2em;">否则，老师一旦和教务处反映，这个数据获取接口就有<span style="color: red;">被关闭</span>的风险！</p>`,
-    async () => {
-      $(root).append(`<div class="sua-container-scores-information"></div>`)
-      showLoadingAnimation('.sua-container-scores-information')
-      const data = await getData()
-      hideLoadingAnimation()
-      $('.sua-container-scores-information').append(indexTemplate(data))
+    async (res: boolean) => {
+      if (res) {
+        $(root).append(`<div class="sua-container-scores-information"></div>`)
+        showLoadingAnimation('.sua-container-scores-information')
+        const data = await getData()
+        hideLoadingAnimation()
+        $('.sua-container-scores-information').append(indexTemplate(data))
+      } else {
+        $(root).append(`
+        <div class="sua-container-scores-information">
+          <p>很抱歉，因为您拒绝了使用协议，SCU URP 助手 无法显示您希望看到的数据。</p>
+        </div>`)
+      }
     }
   )
 }
