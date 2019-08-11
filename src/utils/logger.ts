c8ee7d59977c1b3724ac64d939cf260c54eb01f7
Zhaoji Wang <hwoam@outlook.com>
2019-08-11 10:01:38 +0800

    feat(logger): 输出时显示时间

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,28 +1,30 @@
-const prefix = '%c[SCU URP Assistent]'
+const prefix = () => `%c[SCU URP Assistent](${new Date().toLocaleString()})\n`
 
 function log(...rest: any) {
   if (process.env.NODE_ENV === 'development') {
     const style = 'color: #2EA9DF;'
-    console.log(prefix, style, ...rest)
+    console.log(prefix(), style, ...rest)
   }
 }
 
 function info(...rest: any) {
   if (process.env.NODE_ENV === 'development') {
     const style = 'color: #227D51;'
-    console.log(prefix, style, ...rest)
+    console.log(prefix(), style, ...rest)
   }
 }
+
 function warn(...rest: any) {
   if (process.env.NODE_ENV === 'development') {
     const style = 'color: #F9BF45;'
-    console.log(prefix, style, ...rest)
+    console.log(prefix(), style, ...rest)
   }
 }
+
 function error(...rest: any) {
   if (process.env.NODE_ENV === 'development') {
     const style = 'color: #CB4042;'
-    console.log(prefix, style, ...rest)
+    console.log(prefix(), style, ...rest)
   }
 }
 
