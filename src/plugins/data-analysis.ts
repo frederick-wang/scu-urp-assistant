3ee76c9654071728de2cbda2f893950b3c8e7a89
Zhaoji Wang <hwoam@outlook.com>
2019-08-14 04:21:33 +0800

    feat(data-alanysis): 将版本号做stable和dev的区分

--- src/plugins/data-analysis.ts
+++ src/plugins/data-analysis.ts
@@ -7,14 +7,15 @@ export default {
   pathname: true,
   init() {
     const APP_ID = '36482C98B3E94A4D93A0C66E43702C77'
+    const versionName = `${version} (${
+      process.env.NODE_ENV === 'development' ? 'dev' : 'stable'
+    })`
     const script = document.createElement('script')
-    const src = `https://jic.talkingdata.com/app/h5/v1?appid=${APP_ID}&vc=${version}`
+    const src = `https://jic.talkingdata.com/app/h5/v1?appid=${APP_ID}&vn=${versionName}&vc=${version}`
     script.setAttribute('src', src)
     document.getElementsByTagName('head')[0].appendChild(script)
     script.onload = () => {
-      logger.info(
-        `Data Analysis Plugin Loaded Successfully: ${version}`
-      )
+      logger.info(`Data Analysis Plugin Loaded Successfully: ${versionName}`)
     }
   }
 } as SUAPlugin
