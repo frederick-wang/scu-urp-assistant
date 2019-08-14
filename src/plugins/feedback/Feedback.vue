f0ef0f5eff11ecc6a8928c8b380345e4dcc4ead5
Zhaoji Wang <hwoam@outlook.com>
2019-08-15 07:04:10 +0800

    fix(feedback): 修改VueSPA组件中打错的类名

--- src/plugins/feedback/Feedback.vue
+++ src/plugins/feedback/Feedback.vue
@@ -13,7 +13,7 @@
 import { Vue, Component } from 'vue-property-decorator'
 
 @Component
-export default class ScoresInformation extends Vue {}
+export default class Feedback extends Vue {}
 </script>
 
 <style lang="scss" scoped>
