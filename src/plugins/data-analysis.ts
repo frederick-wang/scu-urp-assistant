12734f4b0a352fcd3ac9c71de9fa714e1600cbc1
Zhaoji Wang <hwoam@outlook.com>
2019-08-18 01:30:26 +0800

    feat(data-analysis): dev版本不再发data-analysis

--- src/plugins/data-analysis.ts
+++ src/plugins/data-analysis.ts
@@ -32,16 +32,18 @@ export default {
     }
   },
   init() {
-    const APP_ID = '36482C98B3E94A4D93A0C66E43702C77'
-    const versionName = `${version} (${
-      process.env.NODE_ENV === 'development' ? 'dev' : 'stable'
-    })`
-    const script = document.createElement('script')
-    const src = `https://jic.talkingdata.com/app/h5/v1?appid=${APP_ID}&vn=${versionName}&vc=${version}`
-    script.setAttribute('src', src)
-    document.getElementsByTagName('head')[0].appendChild(script)
-    script.onload = () => {
-      logger.info(`Data Analysis Plugin Loaded Successfully: ${versionName}`)
+    if (process.env.NODE_ENV !== 'development') {
+      const APP_ID = '36482C98B3E94A4D93A0C66E43702C77'
+      const versionName = `${version} (${
+        process.env.NODE_ENV === 'development' ? 'dev' : 'stable'
+      })`
+      const script = document.createElement('script')
+      const src = `https://jic.talkingdata.com/app/h5/v1?appid=${APP_ID}&vn=${versionName}&vc=${version}`
+      script.setAttribute('src', src)
+      document.getElementsByTagName('head')[0].appendChild(script)
+      script.onload = () => {
+        logger.info(`Data Analysis Plugin Loaded Successfully: ${versionName}`)
+      }
     }
   }
 } as SUAPlugin
