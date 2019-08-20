984a54b374efb4761ec18acb3387299ef1a8d500
Zhaoji Wang <hwoam@outlook.com>
2019-08-20 13:34:39 +0800

    feat(bachelor-degree): 增加专业授位查询功能

--- src/plugins/bachelor-degree/BachelorDegree.vue
+++ src/plugins/bachelor-degree/BachelorDegree.vue
@@ -10,26 +10,37 @@
       .profile-user-info.profile-user-info-striped.self
         .profile-info-row
           .profile-info-name
-            | 专业名称
+            | 请输入专业名称关键字或专业代码
           .profile-info-value
             .profile-info-value
-              input#major(type='text', name='major')
-  .row.result-wrapper(v-if='loadingIsDone')
+              input#major(type='text', name='major' v-model.trim='inputMajor')
+  .row.result-wrapper(v-if='loadingIsDone && similarList.length')
     .col-sm-12
       h4.header.smaller.lighter.grey
         i.menu-icon.fa.fa-table
         |
         | 查询结果
+      p
+        | 以下是根据您输入的关键字在《四川大学学士学位授位专业及授位学科门类表》中查询得到的结果，
+        strong 可能没有一些新专业，欢迎向开发者反馈
+        | 。
       table.table.table-hover.table-bordered.table-striped
         thead
           tr
-            th 专业
-            th 专业名称
-            th 授位学科门类
-            th 批准文号
-            th 备注
+            th.center 序号
+            th.center 专业代码
+            th.center 专业名称
+            th.center 授位学科门类
+            th.center 批准文号
+            th.center 备注
         tbody
-          tr
+          tr(v-for='(v, i) in similarList' :key='v[0]+v[3]')
+            td.center {{ i + 1 }}
+            td.center {{ v[0] }}
+            td.center {{ v[1] }}
+            td.center {{ v[2] }}
+            td.center {{ v[3] }}
+            td.center {{ v[4] }}
 </template>
 
 <script lang="ts">
@@ -37,6 +48,7 @@ import { Vue, Component } from 'vue-property-decorator'
 import { actions, Request } from '@/store'
 import Loading from '@/plugins/common/components/Loading.vue'
 import { emitDataAnalysisEvent } from '../data-analysis'
+import { getTextSimilarity } from '@/utils'
 
 @Component({
   components: { Loading }
@@ -45,6 +57,19 @@ export default class BachelorDegree extends Vue {
   loadingIsDone = false
   // [专业, 专业名称, 授位学科门类, 批准文号, 备注][]
   bachelorDegreeList: string[][] = []
+  inputMajor: string = ''
+
+  get similarList() {
+    return !this.inputMajor
+      ? []
+      : this.bachelorDegreeList.filter(
+          ([majorNumber, majorName]) =>
+            this.inputMajor.split('').every(v => majorName.includes(v)) ||
+            this.inputMajor.split('').every(v => majorNumber.includes(v)) ||
+            getTextSimilarity(majorName, this.inputMajor) > 0.5 ||
+            getTextSimilarity(majorNumber, this.inputMajor) > 0.5
+        )
+  }
 
   async created() {
     try {
@@ -65,5 +90,26 @@ export default class BachelorDegree extends Vue {
 
 .result-wrapper {
   margin-top: 10px;
+}
+.profile-info-row {
+  display: flex;
+
+  .profile-info-name {
+    width: auto;
+    display: flex;
+    align-items: center;
+    padding-right: 20px;
+    padding-left: 20px;
+  }
+  .profile-info-value {
+    flex: 1;
+    display: flex;
+    align-items: center;
+
+    input#major {
+      width: 40% !important;
+      min-width: 200px;
+    }
+  }
 }
 </style>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,48 @@ import * as logger from './logger'
 import { state, actions, Request } from '@/store'
 import { CourseInfoList, TeacherTable, CourseScoreInfo } from '@/store/types'
 import local from '@/store/local'
-import { SemesterScoreRecord } from '@/plugins/score/types';
+import { SemesterScoreRecord } from '@/plugins/score/types'
+
+function getLevenshteinDistance(a: string, b: string) {
+  // Create empty edit distance matrix for all possible modifications of
+  // substrings of a to substrings of b.
+  const distanceMatrix: number[][] = Array(b.length + 1)
+    .fill(null)
+    .map(() => Array(a.length + 1).fill(null))
+
+  // Fill the first row of the matrix.
+  // If this is first row then we're transforming empty string to a.
+  // In this case the number of transformations equals to size of a substring.
+  for (let i = 0; i <= a.length; i += 1) {
+    distanceMatrix[0][i] = i
+  }
+
+  // Fill the first column of the matrix.
+  // If this is first column then we're transforming empty string to b.
+  // In this case the number of transformations equals to size of b substring.
+  for (let j = 0; j <= b.length; j += 1) {
+    distanceMatrix[j][0] = j
+  }
+
+  for (let j = 1; j <= b.length; j += 1) {
+    for (let i = 1; i <= a.length; i += 1) {
+      const indicator = a[i - 1] === b[j - 1] ? 0 : 1
+      distanceMatrix[j][i] = Math.min(
+        distanceMatrix[j][i - 1] + 1, // deletion
+        distanceMatrix[j - 1][i] + 1, // insertion
+        distanceMatrix[j - 1][i - 1] + indicator // substitution
+      )
+    }
+  }
+
+  return distanceMatrix[b.length][a.length]
+}
+
+function getTextSimilarity(str1: string, str2: string) {
+  return (
+    1 - getLevenshteinDistance(str1, str2) / Math.max(str1.length, str2.length)
+  )
+}
 
 function getUserId(studentInfos: Map<string, string>) {
   const name = studentInfos.get('姓名')
@@ -315,5 +356,6 @@ export {
   logger,
   convertCourseInfoListsToTeacherTable,
   getCourseTeacherList,
-  convertCourseScoreInfoListToScoreRecords
+  convertCourseScoreInfoListToScoreRecords,
+  getTextSimilarity
 }
