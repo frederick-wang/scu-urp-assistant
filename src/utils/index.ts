cc8b1fe8f9cf6d5992eb7e15d5ba1bbce2bb4ec4
Zhaoji Wang <hwoam@outlook.com>
2020-04-05 05:10:32 +0800

    refactor(utils): 删除不必要的注释

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,22 +14,14 @@ import { SemesterScoreRecord } from '@/plugins/score/types'
 const API_PATH = process.env.API_PATH
 
 function getLevenshteinDistance(a: string, b: string): number {
-  // Create empty edit distance matrix for all possible modifications of
-  // substrings of a to substrings of b.
   const distanceMatrix: number[][] = Array(b.length + 1)
     .fill(null)
     .map(() => Array(a.length + 1).fill(null))
 
-  // Fill the first row of the matrix.
-  // If this is first row then we're transforming empty string to a.
-  // In this case the number of transformations equals to size of a substring.
   for (let i = 0; i <= a.length; i += 1) {
     distanceMatrix[0][i] = i
   }
 
-  // Fill the first column of the matrix.
-  // If this is first column then we're transforming empty string to b.
-  // In this case the number of transformations equals to size of b substring.
   for (let j = 0; j <= b.length; j += 1) {
     distanceMatrix[j][0] = j
   }
