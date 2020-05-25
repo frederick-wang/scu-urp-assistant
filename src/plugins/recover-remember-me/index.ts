6f516b876e8f0d90140428dc0f58be6ce10218f9
Zhaoji Wang <hwoam@outlook.com>
2020-05-26 05:18:46 +0800

    fix(recover-remember-me): 由于登录界面目前无法获取插件激活状态，所以将自动登录插件设为不可关闭

--- src/plugins/recover-remember-me/index.ts
+++ src/plugins/recover-remember-me/index.ts
@@ -1,11 +1,12 @@
 import { getPluginIcon } from '@/utils'
+import { SUAPlugin } from '@/types'
 
 // 恢复登录页面的「两周之内不必登录」选项插件
 export default {
   name: 'recover-remember-me',
   displayName: '自动登录',
   icon: getPluginIcon('recover-remember-me'),
-  isNecessary: false,
+  isNecessary: true,
   brief:
     '嫌每次访问教务系统都要重新登录太麻烦？该插件可恢复「两周之内不必登录」选项，免除重复登录的劳累。',
   pathname: '/login',
