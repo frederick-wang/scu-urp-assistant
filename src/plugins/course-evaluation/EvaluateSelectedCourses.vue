fcfed523ec47cc0cf418547327afa6dd77120b76
Zhaoji Wang <hwoam@outlook.com>
2020-06-18 18:47:37 +0800

    feat(plugin/course-evaluation): 增加「课程评价」功能初始文件

---
+++ src/plugins/course-evaluation/EvaluateSelectedCourses.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .sua-container-course-evaluation-evaluate-selected-courses
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class EvaluateSelectedCourses extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/plugins/course-evaluation/QueryCourseEvaluation.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .sua-container-course-evaluation-query-course-evaluation
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class QueryCourseEvaluation extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
