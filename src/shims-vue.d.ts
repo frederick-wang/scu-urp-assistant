92b6a080ccb9b5a8d2572b3b5e504f44cb478b73
Zhaoji Wang <hwoam@outlook.com>
2020-06-21 16:54:15 +0800

    feat(shims): 同步Vue的TypeScript Shims和官网最新版本一致

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,16 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  // Global properties can be declared
+  // on the `VueConstructor` interface
+  interface VueConstructor {
+    $myGlobal: string
+  }
+}
+
+// ComponentOptions is declared in types/options.d.ts
+declare module 'vue/types/options' {
+  interface ComponentOptions<V extends Vue> {
+    myOption?: string
+  }
 }
