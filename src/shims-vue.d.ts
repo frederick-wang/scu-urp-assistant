c2a9d5b7814ae88b044a5c26bdc49752c423925d
Zhaoji Wang <hwoam@outlook.com>
2020-06-22 02:48:05 +0800

    fix(shims-vue): 回退到旧的shims

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,16 +1,4 @@
-import Vue from 'vue'
-
-declare module 'vue/types/vue' {
-  // Global properties can be declared
-  // on the `VueConstructor` interface
-  interface VueConstructor {
-    $myGlobal: string
-  }
-}
-
-// ComponentOptions is declared in types/options.d.ts
-declare module 'vue/types/options' {
-  interface ComponentOptions<V extends Vue> {
-    myOption?: string
-  }
+declare module '*.vue' {
+  import Vue from 'vue'
+  export default Vue
 }
