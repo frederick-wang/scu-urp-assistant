e7496ef47ab6d15a54db070c423023ce98a205ce
Zhaoji Wang <hwoam@outlook.com>
2020-06-27 05:35:10 +0800

    feat(helper/util): 增加类型工具Without和XOR

--- src/helper/util.ts
+++ src/helper/util.ts
@@ -97,3 +97,11 @@ export const http = (): AxiosInstance =>
         : {})
     }
   })
+
+export type Without<T, U> = {
+  [P in Exclude<keyof T, keyof U>]?: never
+}
+
+export type XOR<T, U> = T | U extends Record<string, unknown>
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U
