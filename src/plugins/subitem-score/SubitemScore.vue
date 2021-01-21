f1ea3bb6845cdea5135cb807afec900de146f7e8
Zhaoji Wang <hwoam@outlook.com>
2021-01-22 02:53:52 +0800

    feat(plugins/subitem-score): 创建分项成绩查询插件

---
+++ src/plugins/subitem-score/SubitemScore.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.sua-container-subitem-score
+  p 分项成绩查询
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class SubitemScore extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
