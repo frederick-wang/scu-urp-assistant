1296611936185c9083d1cfccfcb6e217d69eb367
Zhaoji Wang <hwoam@outlook.com>
2021-01-29 18:01:14 +0800

    feat(plugins): 增加空白提示页

---
+++ src/plugins/common/components/Tip.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.sua-container-tip
+  p {{ tip }}
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Tip extends Vue {
+  @Prop({
+    type: String,
+    required: true
+  })
+  tip!: string
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
